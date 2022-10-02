9bfba675b06c4f6236cef95442074bc40b96ff61
aulgz <agameronline@gmail.com>
2022-10-02 23:05:10 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <h2>ตารางเรียน สทก.1 2/2565</h2>
+        <hr></hr>
+        <center>
+          <table border='2'>
+            <thead>
+              <tr>
+                <th scope="col">เวลา : วัน</th>
+                <th scope="col">08:20 - 09:20</th>
+                <th scope="col">09:20 - 10:20</th>
+                <th scope="col">10:20 - 11:20</th>
+                <th scope="col">11:20 - 12:20</th>
+                <th scope="col">12:20 - 13:20</th>
+                <th scope="col">13:20 - 14:20</th>
+                <th scope="col">14:20 - 15:20</th>
+                <th scope="col">15:20 - 16:20</th>
+                <th scope="col">16:20 - 16:30</th>
+                <th scope="col">16:30 - 17:30</th>
+                <th scope="col">17:30 - 18:30</th>
+                <th scope="col">18:30 - 19:30</th>
+                <th scope="col">19:30 - 20:30</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">จันทร์</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="row">อังคาร</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="row">พุธ</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="row">พฤหัสบดี</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <th scope="row">ศุกร์</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </center>
+
+      </section>
+    </div>
+  );
+}
+
+export default App;
